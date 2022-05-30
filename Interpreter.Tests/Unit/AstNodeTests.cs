6d29cab281cec7120b78ea1afb068bbb67edaaa1
Stepami <stepami@mail.ru>
2022-05-30 14:07:12 +0300

    fix unit test

--- Interpreter.Tests/Unit/AstNodeTests.cs
+++ Interpreter.Tests/Unit/AstNodeTests.cs
@@ -3,6 +3,7 @@ using Interpreter.Lib.Semantic.Nodes;
 using Interpreter.Lib.Semantic.Nodes.Declarations;
 using Interpreter.Lib.Semantic.Nodes.Statements;
 using Interpreter.Lib.Semantic.Symbols;
+using Interpreter.Lib.Semantic.Types;
 using Moq;
 using Xunit;
 
@@ -13,7 +14,8 @@ namespace Interpreter.Tests.Unit
         [Fact]
         public void PrecedenceTest()
         {
-            var funcSymbol = new Mock<FunctionSymbol>("f", new List<Symbol>());
+            var fType = new Mock<FunctionType>(new Mock<Type>("").Object, new List<Type>());
+            var funcSymbol = new Mock<FunctionSymbol>("f", new List<Symbol>(), fType.Object);
 
             var lexicalDecl = new LexicalDeclaration(false);
             var stmtItemList = new List<StatementListItem>
