7d0b232886d1623aacb7f8aeb9adf3cf1d2b3caf
Stepami <stepami@mail.ru>
2022-09-09 17:06:19 +0300

    перенос DoubleValueConverter в тело инструкции, поскольку он используется только там

--- Interpreter.Lib/IR/Instructions/AsString.cs
+++ Interpreter.Lib/IR/Instructions/AsString.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Interpreter.Lib.VM;
 using Interpreter.Lib.VM.Values;
@@ -35,5 +36,21 @@ namespace Interpreter.Lib.IR.Instructions
         }
 
         protected override string ToStringRepresentation() => $"{Left} = {right.right} as string";
+        
+        private class DoubleValueConverter : JsonConverter<double>
+        {
+            public override bool CanRead => false;
+
+            public override void WriteJson(JsonWriter writer, double value, JsonSerializer serializer) =>
+                // ReSharper disable once CompareOfFloatsByEqualityOperator
+                writer.WriteRawValue(value == Math.Truncate(value)
+                    ? JsonConvert.ToString(Convert.ToInt64(value))
+                    : JsonConvert.ToString(value));
+
+            public override double ReadJson(JsonReader reader, Type objectType,
+                double existingValue, bool hasExistingValue,
+                JsonSerializer serializer) =>
+                throw new NotImplementedException("CanRead is false, so reading is unnecessary");
+        }
     }
 }
