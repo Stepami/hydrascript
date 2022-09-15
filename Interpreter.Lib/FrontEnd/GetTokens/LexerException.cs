aaa2c6cd7fe481272e598a7dd9e0cc480c486edb
Stepami <stepami@mail.ru>
2022-09-16 00:12:42 +0300

    exception guidelines https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/creating-and-throwing-exceptions#defining-exception-classes

--- Interpreter.Lib/FrontEnd/GetTokens/LexerException.cs
+++ Interpreter.Lib/FrontEnd/GetTokens/LexerException.cs
@@ -3,11 +3,16 @@ using Interpreter.Lib.FrontEnd.GetTokens.Data;
 
 namespace Interpreter.Lib.FrontEnd.GetTokens
 {
+    [Serializable]
     public class LexerException : Exception
     {
+        protected LexerException() { }
+        
+        protected LexerException(string message) : base(message) { }
+        
+        protected LexerException(string message, Exception inner) : base(message, inner) { }
+        
         public LexerException(Token token) :
-            base($"Unknown token {token}")
-        {
-        }
+            base($"Unknown token {token}") { }
     }
 }
--- Interpreter.Lib/IR/CheckSemantics/Exceptions/SemanticException.cs
+++ Interpreter.Lib/IR/CheckSemantics/Exceptions/SemanticException.cs
@@ -3,8 +3,15 @@ using Interpreter.Lib.FrontEnd.GetTokens.Data;
 
 namespace Interpreter.Lib.IR.CheckSemantics.Exceptions
 {
+    [Serializable]
     public abstract class SemanticException : Exception
     {
+        protected SemanticException() { }
+        
+        protected SemanticException(string message) : base(message) { }
+        
+        protected SemanticException(string message, Exception inner) : base(message, inner) { }
+        
         protected SemanticException(Segment segment, string message) :
             base($"{segment} {message}") { }
     }
