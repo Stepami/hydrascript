d390af9e64121aff5e1b9382468542a06f742a0c
Stepami <stepami@mail.ru>
2023-01-17 23:54:21 +0300

    removed unused code

--- Interpreter.Lib/IR/Ast/Nodes/StatementListItem.cs
+++ Interpreter.Lib/IR/Ast/Nodes/StatementListItem.cs
@@ -1,6 +1,10 @@
 namespace Interpreter.Lib.IR.Ast.Nodes;
 
-public abstract class StatementListItem : AbstractSyntaxTreeNode
-{
-    public abstract bool IsStatement();
-}
+public abstract class StatementListItem :
+    AbstractSyntaxTreeNode { }
+    
+public abstract class Statement :
+    StatementListItem { }
+    
+public abstract class Declaration :
+    StatementListItem { }
