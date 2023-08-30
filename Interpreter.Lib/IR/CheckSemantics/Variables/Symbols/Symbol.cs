d3a3796d5e4d40d4a762f6eeba8c14a58c74f811
Stepami <Stepami@mail.ru>
2023-08-30 22:35:19 +0300

    symbol state

--- Interpreter.Lib/IR/CheckSemantics/Variables/Symbols/Symbol.cs
+++ Interpreter.Lib/IR/CheckSemantics/Variables/Symbols/Symbol.cs
@@ -4,4 +4,11 @@ public abstract class Symbol
 {
     public abstract string Id { get; }
     public abstract Type Type { get; }
+    public SymbolState State { get; protected set; } = SymbolState.Declared;
+}
+
+public enum SymbolState
+{
+    Declared,
+    Initialized
 }
