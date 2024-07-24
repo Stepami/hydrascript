0aa1a1046a0cd5ab596b077467cad118af86fb75
Sergey Nazarov <insight.appdev@gmail.com>
2024-07-24 13:06:57 +0300

    Add unique Guid check to HashAddress.Equals

---
+++ HydraScript.Tests/Unit/BackEnd/HashAddressTests.cs
@@ -0,0 +1,35 @@
+using HydraScript.Lib.BackEnd.Addresses;
+using Xunit;
+
+namespace HydraScript.Tests.Unit.BackEnd;
+
+public class HashAddressTests
+{
+    [Fact]
+    public void EqualsReturnsFalseForTwoDifferentObjectsWithSameSeed()
+    {
+        const int seed = 1;
+
+        var addressOne = new HashAddress(seed);
+        var addressTwo = new HashAddress(seed);
+
+        Assert.NotEqual(addressOne, addressTwo);
+    }
+    
+    [Fact]
+    public void EqualsReturnsTrueForTwoSameOjectsWithSameSeed()
+    {
+        var address = new HashAddress(1);
+
+        Assert.Equal(address, address);
+    }
+    
+    [Fact]
+    public void EqualsReturnsFalseForTwoObjectsWithDifferentSeed()
+    {
+        var addressOne = new HashAddress(0);
+        var addressTwo = new HashAddress(1);
+
+        Assert.NotEqual(addressOne, addressTwo);
+    }
+}
