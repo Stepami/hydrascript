cf8c8f8317e4552e10bfa241bd34d63bff52c485
Stepami <Stepami@mail.ru>
2024-08-02 18:29:45 +0300

    #57 - token types provider

---
+++ src/Infrastructure/HydraScript.Infrastructure/TokenTypesProvider.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using HydraScript.Domain.FrontEnd.Lexer;
+using HydraScript.Domain.FrontEnd.Lexer.TokenTypes;
+
+namespace HydraScript.Infrastructure;
+
+internal class TokenTypesProvider : ITokenTypesProvider
+{
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        Converters = { new TokenTypesReadConverter() }
+    };
+
+    public IEnumerable<TokenType> GetTokenTypes() =>
+        JsonSerializer.Deserialize<IEnumerable<TokenType>>(
+            TokenTypesJson.String,
+            JsonSerializerOptions)!;
+
+    [ExcludeFromCodeCoverage]
+    private class TokenTypesReadConverter : JsonConverter<IEnumerable<TokenType>>
+    {
+        public override IEnumerable<TokenType> Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            using var jsonDocument = JsonDocument.ParseValue(ref reader);
+            var tokenTypes = jsonDocument.RootElement
+                .EnumerateArray()
+                .Select(element =>
+                {
+                    var tag = element.GetProperty("tag").GetString()!;
+                    var pattern = element.GetProperty("pattern").GetString()!;
+                    var priority = element.GetProperty("priority").GetInt32();
+
+                    var ignorable = element.TryGetProperty("canIgnore", out var canIgnore);
+
+                    return ignorable && canIgnore.GetBoolean()
+                        ? new IgnorableType(tag, pattern, priority)
+                        : new TokenType(tag, pattern, priority);
+                })
+                .OrderBy(x => x.Priority);
+            return tokenTypes;
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            IEnumerable<TokenType> value,
+            JsonSerializerOptions options) => throw new NotSupportedException();
+    }
+}
