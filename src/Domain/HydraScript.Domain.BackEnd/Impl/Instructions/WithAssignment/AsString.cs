9f379c8a9a711b63d612ad79acb48b097af0609b
Степан <stepami@mail.ru>
2024-08-05 23:33:45 +0300

    Перевод на Source Gen Json (#80)
    * #45 - тестовый workflow для проверки AOT publish на инфре GitHub Actions
    
    * #45 - перевёл бекенд на json source gen
    
    * fix
    
    * #45 - перевёл SG на json source gen
    
    * #45 - перевёл Infrastructure на json source gen
    
    * Revert "#45 - тестовый workflow для проверки AOT publish на инфре GitHub Actions"
    
    This reverts commit 3014c53de8d2deb1237254aa345ff514c52d3d05.

--- src/Domain/HydraScript.Domain.BackEnd/Impl/Instructions/WithAssignment/AsString.cs
+++ src/Domain/HydraScript.Domain.BackEnd/Impl/Instructions/WithAssignment/AsString.cs
@@ -3,25 +3,34 @@ using System.Text.Json.Serialization;
 
 namespace HydraScript.Domain.BackEnd.Impl.Instructions.WithAssignment;
 
-public class AsString(IValue value) : Simple(value)
+public partial class AsString(IValue value) : Simple(value)
 {
+    private static readonly AsStringSerializationContext AsStringJsonContext = new(new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
+    });
+    
     public override IAddress Execute(IExecuteParams executeParams)
     {
         var frame = executeParams.Frames.Peek();
         frame[Left!] = JsonSerializer.Serialize(
-            Right.right!.Get(frame),
-            new JsonSerializerOptions
-            {
-                WriteIndented = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                ReferenceHandler = ReferenceHandler.IgnoreCycles,
-                NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
-            }
-        );
+            value: Right.right!.Get(frame)!,
+            AsStringJsonContext.Object);
 
         return Address.Next;
     }
 
     protected override string ToStringInternal() =>
         $"{Left} = {Right.right} as string";
+
+    [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization)]
+    [JsonSerializable(typeof(List<object>))]
+    [JsonSerializable(typeof(Dictionary<string, object>))]
+    [JsonSerializable(typeof(bool))]
+    [JsonSerializable(typeof(double))]
+    [JsonSerializable(typeof(string))]
+    private partial class AsStringSerializationContext : JsonSerializerContext;
 }
