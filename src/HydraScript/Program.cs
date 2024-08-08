bffdaa4c9d33d5decdb54dbb6aa4e1cec2f80da5
Stepami <Stepami@mail.ru>
2024-08-08 14:05:28 +0300

    small fix - return exit code

--- src/HydraScript/Program.cs
+++ src/HydraScript/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-GetRunner(ConfigureHost).Invoke(args);
+return GetRunner(ConfigureHost).Invoke(args);
 
 public static partial class Program
 {
