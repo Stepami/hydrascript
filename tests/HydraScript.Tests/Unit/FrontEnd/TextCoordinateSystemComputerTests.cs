cff1de2ce802d009259ee4541cd373eeeff037da
Stepami <Stepami@mail.ru>
2024-08-20 19:03:41 +0300

    #109 - тесты

--- tests/HydraScript.Tests/Unit/FrontEnd/TextCoordinateSystemComputerTests.cs
+++ tests/HydraScript.Tests/Unit/FrontEnd/TextCoordinateSystemComputerTests.cs
@@ -45,4 +45,27 @@ public class TextCoordinateSystemComputerTests
             stmt1.Length + stmt2.Length + stmt3.Length + Environment.NewLine.Length * 3 - 1
         ]);
     }
+
+    [Theory, MemberData(nameof(EmptySystems))]
+    public void GetCoordinates_EmptySystem_StartCoordinatesReturned(IReadOnlyList<int> newLineList)
+    {
+        var coord = _sut.GetCoordinates(
+            absoluteIndex: Random.Shared.Next(0, int.MaxValue),
+            newLineList);
+        coord.Should().Be(new Coordinates());
+    }
+
+    public static TheoryData<IReadOnlyList<int>> EmptySystems =>
+        new([[], [-1]]);
+
+    [Fact]
+    public void GetCoordinates_NotEmptySystem_CorrectCoordinatesReturned()
+    {
+        // abcd\nabc\naaaa\n
+        //     4    8     13
+        IReadOnlyList<int> newLineList = [-1, 4, 8, 13];
+        // координата символа b
+        var coord = _sut.GetCoordinates(absoluteIndex: 6, newLineList);
+        coord.Should().Be(new Coordinates(Line: 2, Column: 2));
+    }
 }
