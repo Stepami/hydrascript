47211323006d48bc2b5bb9d875000c46eab2aa60
Stepami <stepami@mail.ru>
2024-10-09 22:38:33 +0300

    Внедрение ITestOutputHelper через xUnit DI

---
+++ tests/HydraScript.IntegrationTests/Startup.cs
@@ -0,0 +1,7 @@
+namespace HydraScript.IntegrationTests;
+
+// ReSharper disable once UnusedType.Global
+/// <summary>
+/// https://github.com/pengweiqhca/Xunit.DependencyInjection#4-default-startup
+/// </summary>
+public class Startup;
