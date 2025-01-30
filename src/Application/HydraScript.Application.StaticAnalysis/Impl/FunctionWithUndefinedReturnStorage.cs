98af0beebe102392fcbf346533804b1f788ed605
hw2cb <dim5.dim5.960@gmail.com>
2025-01-31 00:41:34 +0300

    переход на OrderedDictionary

--- src/Application/HydraScript.Application.StaticAnalysis/Impl/FunctionWithUndefinedReturnStorage.cs
+++ src/Application/HydraScript.Application.StaticAnalysis/Impl/FunctionWithUndefinedReturnStorage.cs
@@ -5,13 +5,11 @@ namespace HydraScript.Application.StaticAnalysis.Impl;
 
 internal class FunctionWithUndefinedReturnStorage : IFunctionWithUndefinedReturnStorage
 {
-    private readonly Dictionary<string, FunctionDeclaration> _declarations = [];
-    private readonly Dictionary<string, int> _keysWithOrder = [];
+    private readonly OrderedDictionary<string, FunctionDeclaration> _declarations = [];
 
     public void Save(FunctionSymbol symbol, FunctionDeclaration declaration)
     {
         _declarations[symbol.Id] = declaration;
-        _keysWithOrder[symbol.Id] = _declarations.Count;
     }
 
     public FunctionDeclaration Get(FunctionSymbol symbol)
@@ -19,22 +17,18 @@ internal class FunctionWithUndefinedReturnStorage : IFunctionWithUndefinedReturn
         if (!_declarations.Remove(symbol.Id, out var declaration))
             throw new InvalidOperationException(message: "Cannot get function that has not been saved");
 
-        _keysWithOrder.Remove(symbol.Id);
         return declaration;
     }
 
     public void RemoveIfPresent(FunctionSymbol symbol)
     {
         _declarations.Remove(symbol.Id);
-        _keysWithOrder.Remove(symbol.Id);
     }
 
     public IEnumerable<FunctionDeclaration> Flush() => _declarations
-        .OrderBy(kvp => _keysWithOrder[kvp.Key])
         .Select(x =>
         {
             _declarations.Remove(x.Key);
-            _keysWithOrder.Remove(x.Key);
             return x.Value;
         });
 }
