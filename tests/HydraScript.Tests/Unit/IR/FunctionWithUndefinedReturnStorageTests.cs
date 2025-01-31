75bad60eb65fc671aae090cf72ba7a1541238774
hw2cb <dim5.dim5.960@gmail.com>
2025-02-01 00:09:59 +0300

    добавлен тест для сохранения порядка

--- tests/HydraScript.Tests/Unit/IR/FunctionWithUndefinedReturnStorageTests.cs
+++ tests/HydraScript.Tests/Unit/IR/FunctionWithUndefinedReturnStorageTests.cs
@@ -1,6 +1,7 @@
 using HydraScript.Application.StaticAnalysis;
 using HydraScript.Application.StaticAnalysis.Impl;
 using HydraScript.Domain.FrontEnd.Parser.Impl.Ast.Nodes.Declarations;
+using HydraScript.Domain.IR;
 using HydraScript.Domain.IR.Impl.Symbols;
 using Xunit;
 using BlockStatement = HydraScript.Domain.FrontEnd.Parser.Impl.Ast.Nodes.Statements.BlockStatement;
@@ -11,10 +12,10 @@ namespace HydraScript.Tests.Unit.IR;
 
 public class FunctionWithUndefinedReturnStorageTests
 {
+    const string functionName = nameof(functionName);
     [Fact]
     public void StorageIsEmptyAfterFlushTest()
     {
-        const string functionName = nameof(functionName);
         IFunctionWithUndefinedReturnStorage storage = new FunctionWithUndefinedReturnStorage();
 
         var symbol = new FunctionSymbol(
@@ -35,5 +36,67 @@ public class FunctionWithUndefinedReturnStorageTests
         Assert.Contains(decl, declarations);
         
         Assert.Empty(storage.Flush());
+    }
+
+    [Fact]
+    public void StorageIsCorrectOrderTest()
+    {
+        FunctionDeclaration[] declarations = [
+            new FunctionDeclaration(
+                name: new IdentifierReference(functionName),
+                returnTypeValue: Substitute.For<TypeValue>(),
+                arguments: [],
+                new BlockStatement([])),
+
+            new FunctionDeclaration(
+                name: new IdentifierReference(functionName),
+                returnTypeValue: Substitute.For<TypeValue>(),
+                arguments: [],
+                new BlockStatement([])),
+
+            new FunctionDeclaration(
+                name: new IdentifierReference(functionName),
+                returnTypeValue: Substitute.For<TypeValue>(),
+                arguments: [],
+                new BlockStatement([])),
+
+            new FunctionDeclaration(
+                name: new IdentifierReference(functionName),
+                returnTypeValue: Substitute.For<TypeValue>(),
+                arguments: [],
+                new BlockStatement([]))
+        ];
+        
+        IFunctionWithUndefinedReturnStorage storage = new FunctionWithUndefinedReturnStorage();
+
+        var removable = new FunctionSymbol(
+            id: "key2",
+            parameters: [],
+            "undefined",
+            isEmpty: false);
+
+        storage.Save(new FunctionSymbol(
+            id: "key1",
+            parameters: [],
+            "undefined",
+            isEmpty: false), declaration: declarations[0]);
+
+        storage.Save(removable, declaration: declarations[1]);
+
+        storage.Save(new FunctionSymbol(
+            id: "key3",
+            parameters: [],
+            "undefined",
+            isEmpty: false), declaration: declarations[2]);
+
+        storage.Save(new FunctionSymbol(
+            id: "key4",
+            parameters: [],
+            "undefined",
+            isEmpty: false), declaration: declarations[3]);
+
+        storage.RemoveIfPresent(removable);
+
+        Assert.Equal([declarations[0], declarations[2], declarations[3]], storage.Flush());
     }
 }
