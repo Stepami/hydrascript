813edb958bec63205e76db65ff9db1a4507e6737
admin <levin.8@yandex.ru>
2025-04-02 17:30:35 +0300

    Issues 147 Go at source gen logging

--- src/Infrastructure/HydraScript.Infrastructure/LoggingWriter.cs
+++ src/Infrastructure/HydraScript.Infrastructure/LoggingWriter.cs
@@ -8,12 +8,12 @@ internal partial class LoggingWriter(ILogger<LoggingWriter> logger) : IOutputWri
     [LoggerMessage(
         EventId = 0,
         Level = LogLevel.Information,
-        Message = "`{obj}`")]
+        Message = "{obj}")]
     public partial void WriteLine(object? obj);
 
     [LoggerMessage(
         EventId = 1,
         Level = LogLevel.Error,
-        Message = "`{message}`")]
+        Message = "{message}")]
     public partial void WriteError(Exception e, string message);
 }
