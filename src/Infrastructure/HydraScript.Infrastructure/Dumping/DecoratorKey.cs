b99703f649fcfc31b4c59b15a39c3213ff7145cd
Степан <stepami@mail.ru>
2025-04-02 11:18:42 +0300

    #149 - удаление Scrutor (#159)
    * #149 - remove Scrutor package
    
    * #149 - DI key
    
    * #149 - decorator on MEDI

---
+++ src/Infrastructure/HydraScript.Infrastructure/Dumping/DecoratorKey.cs
@@ -0,0 +1,6 @@
+namespace HydraScript.Infrastructure.Dumping;
+
+public static class DecoratorKey
+{
+    public const string Value = "domain";
+}
--- src/Infrastructure/HydraScript.Infrastructure/Dumping/DumpingVirtualMachine.cs
+++ src/Infrastructure/HydraScript.Infrastructure/Dumping/DumpingVirtualMachine.cs
@@ -1,10 +1,12 @@
 using System.IO.Abstractions;
 using HydraScript.Domain.BackEnd;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace HydraScript.Infrastructure.Dumping;
 
 internal class DumpingVirtualMachine(
+    [FromKeyedServices(DecoratorKey.Value)]
     IVirtualMachine virtualMachine,
     IFileSystem fileSystem,
     IOptions<InputFile> inputFile) : IVirtualMachine
