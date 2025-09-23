6a82824530e511c860575f90bb897ba00fd4e8b0
Stepami <Stepami@mail.ru>
2025-09-23 23:17:56 +0300

    update packages

--- tests/HydraScript.UnitTests/GlobalUsings.cs
+++ tests/HydraScript.UnitTests/GlobalUsings.cs
@@ -1,6 +1,6 @@
 // Global using directives
 
 global using Xunit;
-global using FluentAssertions;
+global using AwesomeAssertions;
 global using Type =  HydraScript.Domain.IR.Types.Type;
 global using NSubstitute;
