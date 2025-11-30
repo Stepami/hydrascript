2ed6ebbe12ed31c4d140e7ea5370d6c5d4310a1d
Stepami <Stepami@mail.ru>
2025-12-01 01:29:02 +0300

    #201 - fix

--- tests/HydraScript.UnitTests/Domain/FrontEnd/ParserData.cs
+++ tests/HydraScript.UnitTests/Domain/FrontEnd/ParserData.cs
@@ -6,13 +6,13 @@ public sealed class ParserSuccessTestData : TheoryData<string>
     {
         Add("-21");
         Add("!false");
-        Add("~[)");
-        Add("x = ([1,2) ++ [3,4))::0");
-        Add("i[0).j");
-        Add("i[0).j()");
+        Add("~[]");
+        Add("x = ([1,2] ++ [3,4])::0");
+        Add("i[0].j");
+        Add("i[0].j()");
         Add("i = 1");
-        Add("i[0) = 1");
-        Add("i[a.b)[1).x(1)");
+        Add("i[0] = 1");
+        Add("i[a.b][1].x(1)");
         Add("(1 + 2) * (3 - (2 / 2)) as string");
         Add("return {x:1;y:2;}");
         Add("while (~arr != 0) { arr::0 continue }");
